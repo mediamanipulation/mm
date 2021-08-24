800b1ee030b221968c6ea75b8e68d766e500862c
Pat McSpaddden <manipulation2000@hotmail.com>
2021-08-24 06:34:44 -0500

    updated photos and text

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,33 +11,33 @@ function Cards() {
           <ul className='cards__items'>
             <CardItem
               src='images/img-9.jpg'
-              text='This is information about pic one.'
-              label='Analog'
+              text='This is information about pic 1.'
+              label='Digital'
               path='/art'
             />
             <CardItem
               src='images/img-2.jpg'
               text='This is information about pic 2'
-              label='Digital'
+              label='Analog'
               path='/art'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
               src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              text='This is information about pic 3'
               label='Digital'
               path='/art'
             />
             <CardItem
               src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
+              text='This is information about pic 4'
               label='Analog'
               path='/products'
             />
             <CardItem
               src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
+              text='This is information about pic 5'
               label='Analog'
               path='/products'
             />
